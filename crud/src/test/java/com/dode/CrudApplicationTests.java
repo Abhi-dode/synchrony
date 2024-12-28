b7fb1ee8cf36fba73283e75038ba8bfb292aa89c
Abhijeet Jivankumar Dode <abhijeet.dode@gyanai.com>
2024-12-28 15:46:58 +0530

    first commet

---
+++ crud/src/test/java/com/dode/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
